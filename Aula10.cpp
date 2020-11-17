01f55667c40806cb3d7973ddf492b852a415e57f
ViniciusTaborda <vinieducosta+1@outlook.com>
2020-11-17 10:06:48 -0300

    10

--- Aula10.cpp
+++ Aula10.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
+
+    bool sunny = false;
+    
+    //exp(T/F) ? T : F 
+
+    string result = sunny ? ":) sunny day" : ":( Not  a sunny day";
+
+    cout << result << endl;
+
+    double balance = 1234.56;
+    double value;
+
+    cout << "Enter the value: ";
+    cin >> value;
+
+    char creditOrDebit;
+    cout << "Credit (c) / Debit (d): ";
+    cin >> creditOrDebit;
+
+    cout << "Balance [Before]: " << balance << endl;
+
+    balance += creditOrDebit == 'c' ? value : -value;
+
+    cout << "Balance [After]: " << balance << endl;
 
 
     return 0;
